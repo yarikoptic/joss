3d4f892b01abaafcbc7ec4d5710972c6820b76a8
Elizabeth DuPre <emd222@cornell.edu>
2021-09-20 20:53:28 -0400

    First stab at editorial board

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,47 @@ Editor.create(
   login: "pbellec",
   email: "pierre.bellec@criugm.qc.ca",
   avatar_url: "https://avatars.githubusercontent.com/u/1670887?v=4",
-  categories: [],
+  categories: ["neuroimaging", "machine learning", "brain connectivity"],
   url: "https://simexp.github.io/lab-website/",
+  description: <<-STR.strip_heredoc()
+  STR
+)
+
+Editor.create(
+  kind: "board",
+  first_name: "Jean-Baptiste",
+  last_name: "Poline",
+  login: "jbpoline",
+  email: "jean-baptiste.poline@mcgill.ca",
+  avatar_url: "https://www.mcgill.ca/neuro/files/neuro/styles/medium_focal__220_x_220_/public/poline_0.jpeg?itok=nSn7FUzq",
+  categories: [],
+  url: "https://neurodatascience.github.io/",
+  description: <<-STR.strip_heredoc()
+  STR
+)
+
+Editor.create(
+  kind: "board",
+  first_name: "Nikola",
+  last_name: "Stikov",
+  login: "nstikov",
+  email: "nikola.stikov@polymtl.ca",
+  avatar_url: "https://avatars.githubusercontent.com/u/29262043?v=4",
+  categories: [],
+  url: "https://www.neuro.polymtl.ca/",
+  description: <<-STR.strip_heredoc()
+  STR
+)
+
+Editor.create(
+  kind: "topic",
+  first_name: "Mathieu",
+  last_name: "Boudreau",
+  login: "mathieuboudreau",
+  email: "mathieu.boudreau2@mail.mcgill.ca",
+  avatar_url: "https://avatars.githubusercontent.com/u/1421029?v=4",
+  categories: [],
+  url: "https://ca.linkedin.com/in/mathieujboudreau",
   description: <<-STR.strip_heredoc()
   STR
 )
@@ -22,7 +61,31 @@ Editor.create(
   categories: ["fMRI", "machine learning"],
   url: "https://elizabeth-dupre.com/",
   description: <<-STR.strip_heredoc()
-  I'm a cognitive neuroscientist at McGill University working to model individual brain activity using high-dimensional, naturalistic data sets.
-  I'm a strong advocate for the role of community-driven science to improve the generalizability of inferences in human brain mapping.
+  STR
+)
+
+Editor.create(
+  kind: "topic",
+  first_name: "Agah",
+  last_name: "Karakuzu",
+  login: "agahkarakuzu",
+  email: "agahkarakuzu@gmail.com",
+  avatar_url: "https://avatars.githubusercontent.com/u/9632322?v=4",
+  categories: [],
+  url: "https://agahkarakuzu.github.io",
+  description: <<-STR.strip_heredoc()
+  STR
+)
+
+Editor.create(
+  kind: "topic",
+  first_name: "Loic",
+  last_name: "Tetrel",
+  login: "ltetrel",
+  email: "loic.tetrel.pro@gmail.com",
+  avatar_url: "https://miro.medium.com/fit/c/262/262/1*opCSBHAJOEB1qmxI-p1n4g.png",
+  categories: [],
+  url: "https://ltetrel.github.io/",
+  description: <<-STR.strip_heredoc()
   STR
 )
